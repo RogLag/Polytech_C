b42a58b50addaf695acdf0e778c3f051a020730a
RogLag <hugo.rog@icloud.com>
2024-09-30 17:28:13 +0200

    ➡️ feat: Added first exercice

---
+++ First_step_in_C.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int sum(int a, int b);
+int product(int a, int b);
+float division(int a, int b);
+
+int main() {
+    printf("Hello, World!\n");
+    int a, b, c, d;
+    float e;
+    printf("Enter two numbers: ");
+    fflush( stdout );
+    scanf("%d %d", &a, &b);
+    c = sum(a, b);
+    printf("%d + %d = %d\n", a, b, c);
+    d = product(a, b);
+    printf("%d x %d = %d\n", a, b, d);
+    e = division(a, b);
+    printf("%d / %d = %f\n", a, b, e);
+    return 0;
+}
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int product(int a, int b) {
+    return a * b;
+}
+
+float division(int a, int b) {
+    return (float) a / b;
+}
